d634de1fdcd3fff9e761900ddb1e8cf579c97bef
Tomás Villaseca <tvvillas@gmail.com>
2016-08-09 18:16:25 -0400

    Enable running a script through an HTTP request

--- kernel-server.py
+++ kernel-server.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import subprocess
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/kernel_test')
+def hello():
+    cmd = ['sh','kernel.sh']
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE)
+    out,err = p.communicate()
+    return out
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
